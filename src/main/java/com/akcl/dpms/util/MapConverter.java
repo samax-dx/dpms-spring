98764bb8505ef364a9f4fe30bda99ea4639a4ebf
samax-dx <samax.dx@aol.com>
2023-07-12 12:22:55 +0600

    added MapConverter

---
+++ src/main/java/com/akcl/dpms/util/MapConverter.java
@@ -0,0 +1,37 @@
+package com.akcl.dpms.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.Optional;
+
+
+@Converter
+public class JsonNodeConverter implements AttributeConverter<JsonNode, String>
+{
+    @Override
+    public String convertToDatabaseColumn(JsonNode jsonNode)
+    {
+        if( jsonNode == null) {
+            return null;
+        }
+
+        return jsonNode.toPrettyString();
+    }
+
+    @Override
+    public JsonNode convertToEntityAttribute(String jsonNodeString) {
+        if (Optional.ofNullable(jsonNodeString).map(v -> v.trim().length() > 0).orElse(false)) {
+            return null;
+        }
+
+        try {
+            return new ObjectMapper().readTree(jsonNodeString);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

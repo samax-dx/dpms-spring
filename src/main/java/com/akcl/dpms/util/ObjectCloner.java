c702150727b78ae9575586110eb0034b0d7b0692
samax-dx <samax.dx@aol.com>
2023-07-22 20:02:04 +0600

    added ObjectCloner

---
+++ src/main/java/com/akcl/dpms/util/ObjectCloner.java
@@ -0,0 +1,17 @@
+package com.akcl.dpms.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ObjectCloner<T> {
+    private final T originalObject;
+
+    public ObjectCloner(T cloneSource) {
+        this.originalObject = cloneSource;
+    }
+
+    T cloneObject(Class<T> tClass) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(objectMapper.writeValueAsString(originalObject), tClass);
+    }
+}
